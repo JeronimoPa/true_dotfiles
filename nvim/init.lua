1a7c033e5d46e10640178370e52cde3934e4cc8b
Negrozma <jeropan04@gmail.com>
2026-03-03 14:48:54 +0100

    nvim para tablet

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+-- options
+require("options")
+
+-- lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- plugins
+require("lazy").setup("plugins")
---
+++ nvim/lua/plugin/lualine.lua
@@ -0,0 +1,5 @@
+-- Lualine
+require("lualine").setup({
+    icons_enabled = true,
+    theme = 'gruvbox',
+})
