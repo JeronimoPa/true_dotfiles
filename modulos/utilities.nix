21d1cb20e737cfd038b46af4c9c3416002262a8b
Jerónimo <jeropan04@gmail.com>
2026-05-13 01:21:47 +0200

    himalaya

--- modulos/utilities.nix
+++ modulos/utilities.nix
@@ -21,6 +21,7 @@
 
 	environment.systemPackages = with pkgs; [
 		keepassxc
+		pass
 
 		xclip
 
--- vim.nix
+++ vim.nix
@@ -15,69 +15,36 @@
 				${builtins.readFile ./modulos/nvim/options.lua}
 			'';
 			extraPackages = with pkgs; [
+				himalaya						#mail
 				nixd							#nix
+				clang-tools						#c y cpp
 				luajitPackages.lua-lsp			#lua
-				ccls
 				pyright							#python
 				jdt-language-server				#java
 				omnisharp-roslyn				#unity
 				vscode-langservers-extracted	#web dev
-				texlab
-
+				texlab							#latex
 				ripgrep							#para live_grep
 			];
 			plugins = with pkgs.vimPlugins; [
+				#markdown
 				markdown-preview-nvim
 				markdown-nvim
 				render-markdown-nvim
+				#fin_markdown
+				#lsp-config
 				{
 					plugin = nvim-lspconfig;
 					config = toLuaFile ./modulos/nvim/plugin/lsp.lua;
 				}
-
-				{
-					plugin = gruvbox-nvim;
-					config = "colorscheme gruvbox";
-				}
-
-				neodev-nvim
-
-				nvim-cmp 
-				
 				{
 					plugin = nvim-cmp;
 					config = toLuaFile ./modulos/nvim/plugin/cmp.lua;
 				}
-
-				{
-					plugin = telescope-nvim;
-					config = toLuaFile ./modulos/nvim/plugin/telescope.lua;
-				}
-
-				{
-					plugin = obsidian-nvim;
-					config = toLuaFile ./modulos/nvim/plugin/obsidian.lua;
-				}
-				{
-					plugin = neo-tree-nvim;
-					config = toLuaFile ./modulos/nvim/plugin/neo-tree.lua;
-				}
 				cmp_luasnip
 				cmp-nvim-lsp
-
 				luasnip
 				friendly-snippets
-
-				{
-					plugin = nvim-autopairs;
-					config = toLuaFile ./modulos/nvim/plugin/autopair.lua;
-				}
-
-				{
-					plugin = lualine-nvim;
-					config = toLuaFile ./modulos/nvim/plugin/lualine.lua;
-				}
-
 				{
 					plugin = (nvim-treesitter.withPlugins (p: [
 						p.tree-sitter-nix
@@ -93,11 +60,36 @@
 					]));
 					config = toLuaFile ./modulos/nvim/plugin/treesitter.lua;
 				}
-
-				# {
-				#   plugin = vimPlugins.own-onedark-nvim;
-				#   config = "colorscheme onedark";
-				# }
+				#fin_lsp-config
+				#estetica
+				{
+					plugin = gruvbox-nvim;
+					config = "colorscheme gruvbox";
+				}
+				{
+					plugin = neo-tree-nvim;
+					config = toLuaFile ./modulos/nvim/plugin/neo-tree.lua;
+				}
+				{
+					plugin = lualine-nvim;
+					config = toLuaFile ./modulos/nvim/plugin/lualine.lua;
+				}
+				#fin_estetica
+				#miscelanea
+				{
+					plugin = telescope-nvim;
+					config = toLuaFile ./modulos/nvim/plugin/telescope.lua;
+				}
+				{
+					plugin = obsidian-nvim;
+					config = toLuaFile ./modulos/nvim/plugin/obsidian.lua;
+				}
+				{
+					plugin = nvim-autopairs;
+					config = toLuaFile ./modulos/nvim/plugin/autopair.lua;
+				}
+				himalaya-vim
+				#fin_miscelanea
 			];
 
 
